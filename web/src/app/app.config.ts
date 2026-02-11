bdb1ae9153206645aab2e89bf4706c9582563dd3
Wolfram Sokollek <wolframsokollek@gmail.com>
2026-02-11 02:27:26 +0100

    feat(web): configure de-DE locale for material datepicker
    - set LOCALE_ID and MAT_DATE_LOCALE to de-DE
    
    - register German locale data and keep native date adapter

--- web/src/app/app.config.ts
+++ web/src/app/app.config.ts
@@ -1,10 +1,16 @@
 import {
   ApplicationConfig,
+  LOCALE_ID,
   provideBrowserGlobalErrorListeners,
   provideZonelessChangeDetection,
 } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
 import { provideHttpClient } from '@angular/common/http';
+import localeDe from '@angular/common/locales/de';
+import { MAT_DATE_LOCALE, provideNativeDateAdapter } from '@angular/material/core';
 import { provideClientHydration, withIncrementalHydration } from '@angular/platform-browser';
+
+registerLocaleData(localeDe);
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,5 +18,8 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     provideClientHydration(withIncrementalHydration()),
     provideZonelessChangeDetection(),
+    provideNativeDateAdapter(),
+    { provide: LOCALE_ID, useValue: 'de-DE' },
+    { provide: MAT_DATE_LOCALE, useValue: 'de-DE' },
   ],
 };
