f4e8b9465c11ba4115ad49ad3390d4173e758431
Wolfram Sokollek <wolframsokollek@gmail.com>
2026-02-11 17:16:48 +0100

    feat(web): add lazy routes and page skeleton for dashboard structure

---
+++ web/src/app/app.routes.ts
@@ -0,0 +1,18 @@
+import { Routes } from '@angular/router';
+
+export const appRoutes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () => import('./stats/shell/stats-dashboard.component').then((m) => m.StatsDashboardComponent),
+  },
+  {
+    path: 'eintraege',
+    loadComponent: () => import('./stats/shell/entries-page.component').then((m) => m.EntriesPageComponent),
+  },
+  {
+    path: 'analyse',
+    loadComponent: () => import('./stats/shell/analysis-page.component').then((m) => m.AnalysisPageComponent),
+  },
+  { path: '**', redirectTo: '' },
+];
