26d7e447b3a6aaff31a1c5ceed98bb0ea289b780
Wolfram Sokollek <wolframsokollek@gmail.com>
2026-02-11 01:21:44 +0100

    refactor(libs): remove unused stats-data-access demo component

--- libs/src/lib/stats-data-access/stats-data-access.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'lib-stats-data-access',
-  imports: [],
-  templateUrl: './stats-data-access.html',
-  styleUrl: './stats-data-access.css',
-})
-export class StatsDataAccess {}
